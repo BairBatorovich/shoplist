1fc73f4b0ff099fc409d97fe725bbacc155a3b14
Bair <pinhaev17@gmail.com>
2020-06-12 15:18:24 +0800

    add sb style

--- screens/styles/index.js
+++ screens/styles/index.js
@@ -7,16 +7,19 @@ import { GRAY, GREEN, RED, WHITE, BLUE } from '../constants';
 const styles = StyleSheet.create({
     list: {
         flex: 1,
-        paddingTop: heightSB,
         justifyContent: 'flex-start',
         alignItems: 'center',
+        // backgroundColor: GRAY
+    },
+    sb: {
+        width: WIDTH,
+        height: heightSB,
         backgroundColor: GRAY
     },
     add: {
         width: WIDTH,
         height: 60,
         paddingLeft: 5,
-        marginTop: 5,
         marginBottom: 7,
         flexDirection: 'row',
         justifyContent: "space-around",
@@ -27,41 +30,30 @@ const styles = StyleSheet.create({
         width: WIDTH - 50,
         height: 40,
         borderRadius: 5,
-        backgroundColor: WHITE
+        backgroundColor: WHITE,
+        paddingLeft: 5
     },
 
     item: {
         width: WIDTH,
         height: 40,
+        marginBottom: 8,
         paddingLeft: 5,
-        marginBottom: 7,
         flexDirection: 'row',
         justifyContent: "space-around",
         alignItems: "center",
-        backgroundColor: WHITE
     },
     itemTxt: {
         width: WIDTH - 50,
         height: 40,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: GRAY,
         textAlign:"center",
         textAlignVertical:"center",
         fontSize: 18,
         fontWeight: "bold",
-    },
-    itemTxtBuy: {
-        width: WIDTH - 50,
-        height: 40,
-        borderWidth: 1,
-        borderRadius: 5,
-        textAlign:"center",
-        textAlignVertical:"center",
-        fontSize: 14,
-    },
-    itemTitle: {
-        width: WIDTH - 50,
-        height: 40,
-        justifyContent:"flex-start",
-        alignItems:"center"
+        backgroundColor: WHITE
     },
 });
 
